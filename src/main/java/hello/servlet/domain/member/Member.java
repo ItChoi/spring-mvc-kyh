b3032d83552f956a3515f5e58bda53ac93fe7ebe
itchoi <itchoi0429@gmail.com>
2021-03-28 23:03:36 +0900

    호우

--- src/main/java/hello/servlet/domain/member/Member.java
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -14,6 +14,8 @@ public class Member {
     private String username;
     private int age;
 
-    public Member(String hello, int i) {
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 }
