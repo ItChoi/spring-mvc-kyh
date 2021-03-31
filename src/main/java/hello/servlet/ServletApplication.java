809e0e6cf3833972eff0da35f8a231550ccd334d
itchoi <itchoi0429@gmail.com>
2021-03-31 23:19:37 +0900

    asd

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -11,5 +14,14 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	/*
+	- 스프링 부트가 아래 뷰 리졸버를 자동으로 등록해준다.
+	- application.properties 파일을 읽어서 prefix, suffix 설정 정보를 등록해준다.
+	@Bean
+	ViewResolver InternalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}
+	*/
 
 }
